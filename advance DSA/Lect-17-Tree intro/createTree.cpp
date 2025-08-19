a47b010d5130856d764539e69fa37d3c9d56f3d8
Vikas Patel <vikaspatel07071999@gmail.com>
2025-08-19 11:00:23 +0530

    tree intro and creation

---
+++ advance DSA/Lect-17-Tree intro/createTree.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data=0;
+    Node *left=NULL;
+    Node* right=NULL;
+    Node(int val){
+        this->data=val;
+    }
+};
+
+void preorder(Node* root){
+      if(root==NULL)
+      return;
+      
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main()
+{
+//   Node* root=new Node(10);
+//   cout<<root->left<<" "<<root->right<<" "<<root->data<<endl;
+
+// Node* rbu=new Node(10);
+// Node* arjun=new Node(9);
+// Node* shivam=new Node(18);
+// rbu->left=shivam;
+// rbu->right=arjun;
+
+// cout<<rbu->right->data;
+
+
+       Node* rbu=new Node(10);
+       rbu->right=  new Node(9);
+       rbu->left= new Node(18);
+      rbu->right->right= new Node(100);
+       rbu->right->left= new Node(200);
+
+    //    dfs->preoprder, inorder, postorder
+    preorder(rbu);
+
+}
